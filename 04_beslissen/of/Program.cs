e0f34215667ebf5dd9a47a9c7dd45b9cd4962a63
OscarvdLubbe <40429@web-ma.nl>
2025-10-02 12:26:48 +0200

    04_beslissen-04

---
+++ 04_beslissen/of/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace of;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kies een fruit: 0 = appel, 1 = banaan, 2 = citroen");
+        int keuze = int.Parse(Console.ReadLine());
+        int appel = 0;
+        int banaan = 1;
+        int citroen = 2;
+        if (keuze == appel || keuze == banaan)
+            Console.WriteLine("Yum Yum");
+        else if (keuze == citroen)
+            Console.WriteLine("Yuck");
+    }
+}
