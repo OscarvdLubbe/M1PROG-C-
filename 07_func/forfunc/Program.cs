91d8f5651f1cee32a355df9f61e170a91b2f5691
OscarvdLubbe <40429@ma-web.nl>
2025-10-30 12:46:20 +0100

    clean

---
+++ 07_func/forfunc/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace forfunc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] basis = new double[] {2, 5, 78, 98};
+
+        for (int i = 0; i < basis.Length; i++)
+        {
+            double Answer = Math.Pow(basis[i], 2);
+            Console.WriteLine($"{basis[i]} keer zichzelf {Answer}");
+        }
+    }
+}
