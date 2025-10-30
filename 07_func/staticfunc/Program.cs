14b32c33b1a8c552787d7e940ff209e49fe06aca
OscarvdLubbe <40429@ma-web.nl>
2025-10-30 13:05:12 +0100

    opd

---
+++ 07_func/staticfunc/Program.cs
@@ -0,0 +1,33 @@
+﻿static void Main(string[] args)
+    {
+        SayHelloWorld();
+        //SayHelloWorld();
+        //SayHelloWorld();
+    }
+
+    static void SayHelloWorld()
+    {
+        Console.WriteLine("Hello World");
+        AfterIntro();
+    }
+    static void StopApp()
+    {
+        Console.WriteLine("Thank you, the app wil close now");
+    }
+    static void AfterIntro()
+    {
+        Console.WriteLine("Would you like to see the intro again? (y/n)");
+        string Choice = Console.ReadLine();
+        if (Choice == "y")
+        {
+            RestartApp();
+        }
+        else if (Choice == "n")
+        {
+            StopApp();
+        }
+    }
+    static void RestartApp()
+    {
+        SayHelloWorld();
+    }
